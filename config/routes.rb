837fe3983d5bcb60dbb53994cd8ba788ca9bedb7
tanay <tanays.tps@gmail.com>
2019-08-13 17:01:16 +0530

    add edit, update, delete for user

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   
   root 'home#index'
   # devise_for :users
-  devise_for :users, :controllers => {:registrations => "users/registrations"}
+  devise_for :users#, :controllers => {:registrations => "users/registrations"}
   resources :roles
-  resources :users
+  resources :users do
+    collection do
+      post :create_user
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

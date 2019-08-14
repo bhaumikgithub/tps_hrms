f3351d93ad47c6947214bb14f8a913aad8de55d1
tanay <tanays.tps@gmail.com>
2019-08-14 11:59:55 +0530

    change in migration and other fixes

--- db/migrate/20190812115204_create_leaves.rb
+++ db/migrate/20190812115204_create_leaves.rb
@@ -3,8 +3,8 @@ class CreateLeaves < ActiveRecord::Migration[5.2]
     create_table :leaves do |t|
       t.string :leave_type
       t.references :user, foreign_key: true
-      t.datetime :date
-      t.string :approved_by
+      t.datetime :leave_date
+      t.references :approved_by, foreign_key: { to_table: :users }
 
       t.timestamps
     end

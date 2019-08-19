feacac937040f538e4f4346e6b053533600364e2
tanay <tanays.tps@gmail.com>
2019-08-19 11:04:26 +0530

    update color for leave types

--- app/models/leave.rb
+++ app/models/leave.rb
@@ -4,7 +4,14 @@ class Leave < ApplicationRecord
 
   # enum leave_type: [:full, :first_half, :second_half, :not_informed, :wfh]
 
-  LEAVE_TYPES =  ["full", "first half", "second half", "not informed", 'wfh'].freeze
+  LEAVE_TYPES =  {"full" => '#0000FF', "first half" => '#000000', "second half" => '#000000', "not informed" => '#FF0000', 'wfh' => '#008000'}.freeze
+
+  before_save :update_color
+
+  def update_color
+    self.color = Leave::LEAVE_TYPES[self.leave_type.downcase]
+    true
+  end
 
 
 end

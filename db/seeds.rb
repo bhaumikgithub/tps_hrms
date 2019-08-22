dcb42a6321458091f50c6420c451c8c2680e1474
nency <nency.tps@gmail.com>
2019-08-22 19:07:46 +0530

    add cancel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+role = Role.create(name: "Admin")
+Role.create(name: "User")
 company = Company.create(comp_email: 'tps@gmail.com', comp_name: 'tps', subdomain: 'tsp')
-User.create(email: 'admin@gmail.com', password: "123456", first_name: 'admin', company: company)
-User.create(email: 'nency@gmail.com', password: "123456", first_name: 'nency')
-User.create(email: 'kinjal@gmail.com', password: "123456",first_name: 'kinjal')
-User.create(email: 'tanay@gmail.com', password: "123456",first_name: 'tanay')
-User.create(email: 'parul@gmail.com', password: "123456",first_name: 'parul')
-User.create(email: 'neha@gmail.com', password: "123456",first_name: 'neha')
+User.create(email: 'admin@gmail.com', password: "123456", first_name: 'admin', role_id: 'role.id')

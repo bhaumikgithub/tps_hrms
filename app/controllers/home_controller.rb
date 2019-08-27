8a8301b852d77dc47307419fa8d2f3153a51b240
nency <nency.tps@gmail.com>
2019-08-27 19:03:41 +0530

    emp handbook

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def employee_handbook
+  end
 end
--- app/models/leave.rb
+++ app/models/leave.rb
@@ -3,7 +3,7 @@ class Leave < ApplicationRecord
   belongs_to :approved_by,class_name: 'User', foreign_key: 'approved_by_id'
 
   # enum leave_type: [:full, :first_half, :second_half, :not_informed, :wfh]
-
+  LEAVE_STATUS = ["pending", "approved"]
   LEAVE_TYPES =  {"full" => '#0000FF', "first half" => '#000000', "second half" => '#000000', "not informed" => '#FF0000', 'wfh' => '#008000'}.freeze
 
   before_save :update_color

62d6eb13b36711e61f2dee936dbd7f6059212ffc
tanay <tanays.tps@gmail.com>
2019-09-03 10:35:28 +0530

    add sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,11 +70,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.sendgrid.net',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            ENV['USER_EMAIL'],
-    password:             ENV['USER_PASSWORD'],
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }

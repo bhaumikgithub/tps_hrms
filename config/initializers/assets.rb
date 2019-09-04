fac08d0d34830f3a7b993cdd03ea45554f5b4000
tanay <tanays.tps@gmail.com>
2019-09-04 10:29:33 +0530

    add ckeditor model assests

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,10 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
+Rails.application.config.assets.precompile += Ckeditor.assets
 Rails.application.config.assets.precompile += %w( ckeditor/*)
+Rails.application.config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.

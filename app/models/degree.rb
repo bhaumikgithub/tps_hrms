7e1f65e3dc8ad533637460315b53dd5fbc005e9b
nency <nency.tps@gmail.com>
2019-10-15 17:40:41 +0530

    add validation

--- app/models/degree.rb
+++ app/models/degree.rb
@@ -1,3 +1,5 @@
 class Degree < ApplicationRecord
   has_many :users
+  validates_presence_of :name
+
 end
--- app/models/designation.rb
+++ app/models/designation.rb
@@ -1,3 +1,4 @@
 class Designation < ApplicationRecord
   has_many :users
+  validates_presence_of :name
 end

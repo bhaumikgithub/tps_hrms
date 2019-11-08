77ce99e76284ae0371944cc855aadf233ad4d47e
nency <nency.tps@gmail.com>
2019-11-08 16:11:00 +0530

    order changes

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -2,7 +2,7 @@ class HolidaysController < ApplicationController
   include InheritAction
 
    def index
-    @resources = resource_class.all.order('holiday_date DESC').page(params[:page]).per(10)
+    @resources = resource_class.all.order('holiday_date ASC').page(params[:page]).per(10)
     respond_to do |format|
       format.html
       format.json { render json: @resources }

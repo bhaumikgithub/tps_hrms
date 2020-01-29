4f595b1c79012811c3924da5f89c95abfe320062
parul kanani <parulk.tps@gmail.com>
2020-01-29 12:14:36 +0530

    added condition

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,9 @@ class ContactsController < ApplicationController
     c = SuggestionForm.new(:name => current_user.full_name, :email => current_user.email,comments: params[:comment], title: params[:title])
     if c.valid?
       c.deliver
-      redirect_to root_url, notice: "Thank you for suggestions"    
+      redirect_to root_path, notice: "Thank you for suggestions" 
+    else
+      redirect_to root_path, alert: "Something want wrong!"
     end
   end
 end

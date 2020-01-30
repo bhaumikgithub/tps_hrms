0b859c91b9c30710947a7ed419f3364806c958e9
parul kanani <parulk.tps@gmail.com>
2020-01-30 10:13:47 +0530

    Added required in select box

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,7 @@ class ContactsController < ApplicationController
     c = SuggestionForm.new(:name => current_user.full_name, :email => current_user.email,comments: params[:comment], title: params[:title])
     if c.valid?
       c.deliver
-      redirect_to root_path, notice: "Thank you for suggestions" 
+      redirect_to root_path, notice: "Thanks for your suggestions." 
     else
       redirect_to root_path, alert: "Something went wrong! Please verify your email."
     end

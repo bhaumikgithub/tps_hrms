d7f98cafe4b6db805389237983467b64b630e867
parul kanani <parulk.tps@gmail.com>
2020-07-09 10:48:27 +0530

    Disable selected date on assets

--- app/models/asset_history.rb
+++ app/models/asset_history.rb
@@ -2,7 +2,7 @@ class AssetHistory < ApplicationRecord
   belongs_to :user
   belongs_to :asset
 
-  scope :activeDate, -> { where("? BETWEEN from_date AND to_date", Time.now.to_date)}
+  # scope :activeDate, -> { where("? BETWEEN from_date AND to_date", Time.now.to_date)}
   scope :activeAtDate, lambda{ |date = Date.today| where("? BETWEEN from_date AND to_date", date) }
 
 end
